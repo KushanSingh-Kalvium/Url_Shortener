6f42b74aae7c3eda684c574bbada98d308200aa2
KushanSingh-Kalvium <kushansingh890@gmail.com>
2025-11-12 09:42:29 +0530

    I have intialized frontend and move the backend files in their respective folders

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise')
 const { nanoid } = require('nanoid');
+const redis = require('redis');
 
 const app = express();
 app.use(express.json());
@@ -16,13 +17,21 @@ async function connectDB() {
     return connection;
 }
 
+async function connectRedis() {
+    const client = redis.createClient();
+    client.on('error', (err) => console.error('❌ Redis connection error:', err));
+    await client.connect();
+    console.log("🚀 Redis successfully connected");
+    return client;
+}
+
 app.get('/', async(req, res) => {
     res.send("✔️ Backend successfully connected");
 });
 
 app.get('/urls', async(req, res) => {
     try {
-        const [rows] = await db.execute('select * from urls');
+        const [rows] = await db.execute('select * from urls order by created_at DESC');
         res.json(rows);
     } catch(err) {
         console.error('❌ Errors fetching URLs:', err);
@@ -55,9 +64,17 @@ app.post('/shorten', async(req, res) => {
 app.get('/:shortCode', async(req, res) => {
     try {
         const {shortCode} = req.params;
+        const cachedUrl = await redisClient.get(shortCode);
+        if (cachedUrl) {
+            console.log('⚡ Cache hit', shortCode);
+            return res.redirect(cachedUrl);
+        }
         const [rows] = await db.execute('select long_url from urls where short_code = ?', [shortCode]);
         if(rows.length>0) {
-                res.redirect(rows[0].long_url);
+            const longUrl = rows[0].long_url;
+            await redisClient.set(shortCode, longUrl, {EX: 3600});
+            console.log('🧠 Cache miss — added to Redis:', shortCode);
+                res.redirect(longUrl);
         } else {
             res.status(404).json({error: 'Short url not found'});
         }
@@ -67,11 +84,15 @@ app.get('/:shortCode', async(req, res) => {
     }
 });
 
-let db;
+let db, redisClient;
 
-connectDB()
-    .then((connection) => {
-        db = connection;
+Promise.all ([connectDB(), connectRedis()])
+    .then(([mysqlconnection, redisConnection]) => {
+        db = mysqlconnection;
+        redisClient = redisConnection;
         app.listen(3000, () => console.log("Server running at http://localhost:3000"))
     })
-    .catch((err) => console.error("❌ MySQL connection failed", err));
+    .catch((err) => console.error("❌ Intialization failed", err));
+
+
+    
